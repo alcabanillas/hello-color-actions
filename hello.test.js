e61a29d06819cdd1549ced6a4704d8f3dae4a491
alcabanillas <acabanillas@gmail.com>
2023-03-05 10:36:53 +0100

    cambio politicas s3

--- hello.test.js
+++ hello.test.js
@@ -1,5 +1,5 @@
 const hello = require("./hello");
-describe("My hello", () => {
+describe("My hello again", () => {
   test("works", () => {
     expect(hello.hello()).toEqual("Hello World");
   });
